1f468a3a4c0a518a9ed33fafd5fa73a1701257ff
David Beaumont <david@zorq.net>
2012-12-23 18:23:59 +0000

    Add a species class

---
+++ speciesextractor/species.py
@@ -0,0 +1,10 @@
+class Species:
+	"""
+	Representation of a species
+	"""
+
+	def __init__(self, binomial_name):
+		"""
+		Construct a species from its binomial name
+		"""
+		self.binomial_name = binomial_name
