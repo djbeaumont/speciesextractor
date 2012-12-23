7ebfa50c598471fc3826a53236a2b86e3e19442a
David Beaumont <david@zorq.net>
2012-12-23 20:56:19 +0000

    Add __str__ methods to Species

--- speciesextractor/species.py
+++ speciesextractor/species.py
@@ -1,10 +1,14 @@
 class Species:
-	"""
-	Representation of a species
-	"""
+	"""Representation of a species"""
 
 	def __init__(self, binomial_name):
-		"""
-		Construct a species from its binomial name
-		"""
+		"""Construct a species from its binomial name"""
 		self.binomial_name = binomial_name
+
+	def __str__(self):
+		"""String representation of this Species"""
+		return 'Species: %s' % self.binomial_name
+
+	def __repr__(self):
+		"""String representation of this Species for a REPL"""
+		return '<%s>' % self.__str__()
