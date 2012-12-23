0a38e8c54205079c72dd2d940a8fcf4c95df9fa4
David Beaumont <david@zorq.net>
2012-12-23 21:02:24 +0000

    Format docstrings to take less space

--- speciesextractor/download.py
+++ speciesextractor/download.py
@@ -1,9 +1,8 @@
 import urllib, tempfile, bz2, os
 
 def download():
-	"""
-	Download a dump file and store it in the user's temporary folder
-	"""
+	"""Download a dump file and store it in the user's temporary folder"""
+
 	tmp_dir = tempfile.gettempdir()
 	web_location = "http://dumps.wikimedia.org/specieswiki/20121213/specieswiki-20121213-pages-meta-current.xml.bz2"
 	web_filename = web_location.split('/')[-1]
@@ -15,9 +14,8 @@ def download():
 	return decompressed_location
 
 def extract(local_location):
-	"""
-	Extract the given compressed file
-	"""
+	"""Extract the given compressed file"""
+	
 	decompressed_location = local_location[:-4]
 
 	data = bz2.BZ2File(local_location).read()
--- speciesextractor/parser.py
+++ speciesextractor/parser.py
@@ -3,22 +3,17 @@ from .species import Species
 import re
 
 class Parser:
-	"""
-	Parser to read XML files and represent data as python objects
-	"""
+	"""Parser to read XML files and represent data as python objects"""
 
 	def __init__(self, local_location):
-		"""
-		Construct a parser with the XML file's local location
-		"""
+		"""Construct a parser with the XML file's local location"""
+
 		self.local_location = local_location
 		self.ns = {'n': 'http://www.mediawiki.org/xml/export-0.8/'}
 		self.all_species = []
 
 	def parse(self):
-		"""
-		Read XML out of a file and produce an object tree of species
-		"""
+		"""Read XML out of a file and produce an object tree of species"""
 		
 		page_tag = '{%s}%s' % (self.ns['n'], 'page')
 		for event, element in etree.iterparse(self.local_location):
@@ -37,26 +32,21 @@ class Parser:
 				element.clear()
 
 	def is_species_page(self, title, text):
-		"""
-		Does this page contain a species with a standardised binomial name
-		"""
+		"""Does this page contain a species with a standardised binomial name"""
+
 		return self.is_binomial_form(title) and self.has_taxonavigation(text)
 
 	def is_template_page(self, title, text):
-		"""
-		Is this page a template
-		"""
+		"""Is this page a template"""
+
 		# TODO
 		return False
 
 	def is_binomial_form(self, title):
-		"""
-		Check if the page title matches the binomial naming format
-		"""
+		"""Check if the page title matches the binomial naming format"""
 		return re.match('^[A-Z]{1}[a-z]+ [a-z]+$', title) != None
 
 	def has_taxonavigation(self, text):
-		"""
-		Check if the page text contains a 'Taxonavigation' section
-		"""
+		"""Check if the page text contains a 'Taxonavigation' section"""
+		
 		return re.search('==[\s]?Taxonavigation[\s]?==', text) != None
