e7745198ec8d852dcc16b50a6c2f521297ea8188
David Beaumont <david@zorq.net>
2013-01-01 02:31:21 +0000

    Add stub configuration options for target database

--- speciesextractor/extractor.py
+++ speciesextractor/extractor.py
@@ -7,7 +7,27 @@ def main():
     """Entry point into the application. Parse options and arguments."""
 
     argparser = argparse.ArgumentParser(description='Download, extract and import species data.')
-    argparser.add_argument('location', metavar='L', type=str, help='location of the wikispecies dump')
+    argparser.add_argument('location',
+                            metavar='L',
+                            type=str,
+                            help='location of the wikispecies dump')
+
+    databases = argparser.add_mutually_exclusive_group(required=True)
+    databases.add_argument('--sqlite', help='save species to a SQLite database')
+    databases.add_argument('--postgres', help='save species to a PostgreSQL database')
+
+    sqliteopts = argparser.add_argument_group(
+        'SQLite Options', 'Configuration of saving species to a SQLite database')
+    sqliteopts.add_argument('-o', '--output',
+                            metavar='FILENAME',
+                            type=str,
+                            help='filename for new SQLite species database')
+
+    postgresopts = argparser.add_argument_group(
+        'PostgreSQL Options', 'Configuration of saving species to a PostgreSQL database')
+    postgresopts.add_argument('-d', '--database', help='name of the database to insert species data into')
+    postgresopts.add_argument('-u', '--username', help='username to connect to the database server')
+    postgresopts.add_argument('-p', '--password', help='password to connect to the database server')
 
     args = argparser.parse_args()
 
